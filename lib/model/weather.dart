4a94aa4af96f828c73389750fbdf020080c58807
Faith Adetunji Komolafe <komolafefaith@gmail.com>
2023-02-21 23:35:26 +0100

    Refactor: Refactored Apps Model class

--- lib/model/weather.dart
+++ lib/model/weather.dart
@@ -1,78 +1,98 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/model/temperature.dart';
 import 'package:weather_app/utilities/WeatherIconMapper.dart';
-import 'package:weather_app/utilities/converters.dart';
+
+
+class WeatherResponse {
+  final Coord? coord;
+  final List<Weather>? weather;
+  final String? base;
+  final Main? main;
+  final int? visibility;
+  final Wind? wind;
+  final Clouds? clouds;
+  final int? dt;
+  final Sys? sys;
+  final int? timezone;
+  final int? id;
+  final String? name;
+  final int? cod;
+
+  WeatherResponse({
+    this.coord,
+    this.weather,
+    this.base,
+    this.main,
+    this.visibility,
+    this.wind,
+    this.clouds,
+    this.dt,
+    this.sys,
+    this.timezone,
+    this.id,
+    this.name,
+    this.cod,
+  });
+
+  factory WeatherResponse.fromJson(Map<String, dynamic> json) {
+    return WeatherResponse(
+      coord: json['coord'] == null ? null : Coord.fromJson(json['coord']),
+      weather: json['weather'] == null ? null : (json['weather'] as List<dynamic>).map((w) => Weather.fromJson(w)).toList(),
+      base: json['base'],
+      main: json['main'] == null ? null : Main.fromJson(json['main']),
+      visibility: json['visibility'],
+      wind: json['wind'] == null ? null : Wind.fromJson(json['wind']),
+      clouds: json['clouds'] == null ? null : Clouds.fromJson(json['clouds']),
+      dt: json['dt'],
+      sys: json['sys'] == null ? null : Sys.fromJson(json['sys']),
+      timezone: json['timezone'],
+      id: json['id'],
+      name: json['name'],
+      cod: json['cod'],
+    );
+  }
+}
+
+class Coord {
+  final double? lon;
+  final double? lat;
+
+  Coord({
+    this.lon,
+    this.lat,
+  });
+
+  factory Coord.fromJson(Map<String, dynamic> json) {
+    return Coord(
+      lon: json['lon'],
+      lat: json['lat'],
+    );
+  }
+}
 
 class Weather {
-  int? id;
-  int? time;
-  int? sunrise;
-  int? sunset;
-  int? humidity;
+  final int? id;
+  final String? main;
+  final String? description;
+  final String? icon;
 
-  String? description;
-  String? iconCode;
-  String? main;
-  String? cityName;
+  Weather({
+    this.id,
+    this.main,
+    this.description,
+    this.icon,
+  });
 
-  double? windSpeed;
-
-  Temperature? temperature;
-  Temperature? maxTemperature;
-  Temperature? minTemperature;
-
-  List<Weather>? forecast;
-
-  Weather(
-      {this.id,
-      this.time,
-      this.sunrise,
-      this.sunset,
-      this.humidity,
-      this.description,
-      this.iconCode,
-      this.main,
-      this.cityName,
-      this.windSpeed,
-      this.temperature,
-      this.maxTemperature,
-      this.minTemperature,
-      this.forecast});
-
-  static Weather fromJson(Map<String, dynamic> json) {
-    final weather = json['weather'][0];
+  factory Weather.fromJson(Map<String, dynamic> json) {
     return Weather(
-      id: weather['id'],
-      time: json['dt'],
-      description: weather['description'],
-      iconCode: weather['icon'],
-      main: weather['main'],
-      cityName: json['name'],
-      temperature: Temperature(intToDouble(json['main']['temp'])),
-      maxTemperature: Temperature(intToDouble(json['main']['temp_max'])),
-      minTemperature: Temperature(intToDouble(json['main']['temp_min'])),
-      sunrise: json['sys']['sunrise'],
-      sunset: json['sys']['sunset'],
-      humidity: json['main']['humidity'],
-      windSpeed: intToDouble(json['wind']['speed']),
+      id: json['id'],
+      main: json['main'],
+      description: json['description'],
+      icon: json['icon'],
     );
   }
 
-  static List<Weather> fromForecastJson(Map<String, dynamic> json) {
-    final weathers = <Weather>[];
-    for (final item in json['list']) {
-      weathers.add(Weather(
-          time: item['dt'],
-          temperature: Temperature(intToDouble(
-            item['main']['temp'],
-          )),
-          iconCode: item['weather'][0]['icon']));
-    }
-    return weathers;
-  }
-
   IconData getIconData() {
-    switch (iconCode) {
+    switch (icon) {
       case '01d':
         return WeatherIcons.clear_day;
       case '01n':
@@ -110,5 +130,88 @@ class Weather {
       default:
         return WeatherIcons.clear_day;
     }
+  }
+}
+
+class Main {
+  final double? temp;
+  final double? feelsLike;
+  final double? tempMin;
+  final double? tempMax;
+  final int? pressure;
+  final int? humidity;
+  final int? seaLevel;
+  final int? grndLevel;
+
+  Main({
+    this.temp,
+    this.feelsLike,
+    this.tempMin,
+    this.tempMax,
+    this.pressure,
+    this.humidity,
+    this.seaLevel,
+    this.grndLevel,
+  });
+
+  factory Main.fromJson(Map<String, dynamic> json) {
+    return Main(
+      temp: json['temp'],
+      feelsLike: json['feels_like'],
+      tempMin: json['temp_min'],
+      tempMax: json['temp_max'],
+      pressure: json['pressure'],
+      humidity: json['humidity'],
+      seaLevel: json['sea_level'],
+      grndLevel: json['grnd_level'],
+    );
+  }
+}
+
+class Wind {
+  final double? speed;
+  final int? deg;
+  final double? gust;
+
+  Wind({this.speed, this.deg, this.gust});
+
+  factory Wind.fromJson(Map<String, dynamic> json) {
+    return Wind(
+      speed: json['speed']?.toDouble(),
+      deg: json['deg'],
+      gust: json['gust']?.toDouble(),
+    );
+  }
+}
+
+class Clouds {
+  final int? all;
+
+  Clouds({this.all});
+
+  factory Clouds.fromJson(Map<String, dynamic> json) {
+    return Clouds(
+      all: json['all'],
+    );
+  }
+}
+
+class Sys {
+  final int? type;
+  final int? id;
+  final String? country;
+  final int? sunrise;
+  final int? sunset;
+
+  Sys({this.type, this.id, this.country, this.sunrise, this.sunset});
+
+  factory Sys.fromJson(Map<String, dynamic> json) {
+    return Sys(
+      type: json['type'],
+      id: json['id'],
+      country: json['country'],
+      sunrise: json['sunrise'],
+      sunset: json['sunset'],
+    );
   }
 }
